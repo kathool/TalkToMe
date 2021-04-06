84e3cf3f84758f5ebd38d5e8cf61534d2e93adfb
Nicholas Daggett <ntdaggett@gmail.com>
2021-04-06 19:38:52 -0400

    Update baselevel.py

--- baselevel.py
+++ baselevel.py
@@ -13,7 +13,31 @@ async def main(int_tries=3):
     await asyncio.sleep(2)
     print('\nFinished!')
     await asyncio.sleep(2)
-    # Put the questions here!
+    # First-Time Question
+    QUESTIONNAMEGOESHERE = input('\n\QUESTION GOES HERE\n\n')
+    file = open("QUESTIONNAMEGOESHERE.txt", "w")
+    print('*writes that down*')
+    file.write(color)
+    await asyncio.sleep(1)
+    print('Go on...')
+    # Repeated Question
+    q = input('QUESTION GOES HERE\n\n')
+    file = open("QUESTIONNAMEGOESHERE.txt")
+    answer = file.read().replace("\n", " ")
+    file.close()
+    while q:
+        if q in answer:
+            print('Go on...')
+            break
+        else:
+            print('You are not that great at lying...')
+            if tries == 0:
+                quit()
+            else:
+                tries = tries - 1
+                print('You have ' + str(tries) + ' left!')
+                break
+    await asyncio.sleep(1)
     # ends level
     await asyncio.sleep(1)
     print("\nYour free to go.\n\nYou won! Victory!\n\n\n")
